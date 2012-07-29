d62b63c70c6606fe08a7b5727fea4d8255be867a
mstiles92 <mstiles92@gmail.com>
2012-07-29 00:46:35 -0400

    First working version

--- src/com/mstiles92/nogolems/NoGolemsEventListener.java
+++ src/com/mstiles92/nogolems/NoGolemsEventListener.java
@@ -1,14 +1,47 @@
 package com.mstiles92.nogolems;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 public class NoGolemsEventListener implements Listener {
 	
 	@EventHandler(priority = EventPriority.NORMAL)
-	public void onGolemBuild(CreatureSpawnEvent e) {
+	public void onGolemBuild(BlockPlaceEvent e) {
+		if (e.isCancelled()) return;
+		if ((e.getBlock() == null) || (e.getPlayer() == null)) return;
+		if ((e.getBlock().getType() != Material.PUMPKIN) && (e.getBlock().getType() != Material.JACK_O_LANTERN)) return;
 		
+		Location location = e.getBlock().getLocation();
+		
+		// Check for Snow Golem
+		if ((location.getWorld().getBlockAt(location.getBlockX(), location.getBlockY() - 1, location.getBlockZ()).getType() == Material.SNOW_BLOCK)
+				&& (location.getWorld().getBlockAt(location.getBlockX(), location.getBlockY() - 2, location.getBlockZ()).getType() == Material.SNOW_BLOCK)) {
+			if (!e.getPlayer().hasPermission("nogolems.allow.snow")) {
+				e.getPlayer().sendMessage(ChatColor.RED + "You do not have permission to create Snow Golems.");
+				e.setCancelled(true);
+				return;
+			}
+			else return;
+		}
+		
+		// Check for Iron Golem
+		if ((location.getWorld().getBlockAt(location.getBlockX(), location.getBlockY() - 1, location.getBlockZ()).getType() == Material.IRON_BLOCK)
+				&& (location.getWorld().getBlockAt(location.getBlockX(), location.getBlockY() - 2, location.getBlockZ()).getType() == Material.IRON_BLOCK)
+				&& ( (location.getWorld().getBlockAt(location.getBlockX() + 1, location.getBlockY() - 1, location.getBlockZ()).getType() == Material.IRON_BLOCK) &&
+				(location.getWorld().getBlockAt(location.getBlockX() - 1, location.getBlockY() - 1, location.getBlockZ()).getType() == Material.IRON_BLOCK)) 
+				|| ((location.getWorld().getBlockAt(location.getBlockX(), location.getBlockY() - 1, location.getBlockZ() - 1).getType() == Material.IRON_BLOCK)
+				&& (location.getWorld().getBlockAt(location.getBlockX(), location.getBlockY() - 1, location.getBlockZ() + 1).getType() == Material.IRON_BLOCK))) {
+			if (!e.getPlayer().hasPermission("nogolems.allow.iron")) {
+				e.getPlayer().sendMessage(ChatColor.RED + "You do not have permission to create Iron Golems.");
+				e.setCancelled(true);
+				return;
+			}
+			else return;
+		}
 	}
 }
