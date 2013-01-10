e0dda662ffe19714ab95da2ae69325b67ba92708
mstiles92 <mstiles92@gmail.com>
2013-01-10 15:00:51 -0500

    Move listener method into main plugin class

--- src/com/mstiles92/nogolems/NoGolemsPlugin.java
+++ src/com/mstiles92/nogolems/NoGolemsPlugin.java
@@ -1,23 +1,57 @@
 package com.mstiles92.nogolems;
 
-import java.util.logging.Logger;
-
-import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NoGolemsPlugin extends JavaPlugin {
-	
-	private PluginDescriptionFile pdf;
-	private Logger log;
-	
+public class NoGolemsPlugin extends JavaPlugin implements Listener {
 	public void onEnable() {
-		this.pdf = this.getDescription();
-		this.log = this.getLogger();
-		log.info(pdf.getName() + " by " + pdf.getAuthors().get(0) + " version " + pdf.getVersion() + " enabled!");
-		this.getServer().getPluginManager().registerEvents(new NoGolemsEventListener(), this);
+		this.getServer().getPluginManager().registerEvents(this, this);
 	}
 	
 	public void onDisable() {
-		log.info(pdf.getName() + " by " + pdf.getAuthors().get(0) + " version " + pdf.getVersion() + " disabled!");
+		
+	}
+	
+	@EventHandler(ignoreCancelled = true)
+	public void onBlockPlace(BlockPlaceEvent e) {
+		if (e.getBlock() == null || e.getPlayer() == null) {
+			return;
+		}
+		
+		final Block b = e.getBlock();
+		
+		if (b.getType() != Material.PUMPKIN && b.getType() != Material.JACK_O_LANTERN) {
+			return;
+		}
+		
+		// Check for snow golem
+		if (b.getRelative(BlockFace.DOWN).getType() == Material.SNOW_BLOCK
+				&& b.getRelative(BlockFace.DOWN).getRelative(BlockFace.DOWN).getType() == Material.SNOW_BLOCK) {
+			if (!e.getPlayer().hasPermission("nogolems.allow.snow")) {
+				e.getPlayer().sendMessage(ChatColor.RED + "You do not have permission to build a Snow Golem.");
+				e.setCancelled(true);
+			}
+			return;
+		}
+		
+		// Check for Iron Golem
+		if (b.getRelative(BlockFace.DOWN).getType() == Material.IRON_BLOCK
+				&& b.getRelative(BlockFace.DOWN).getRelative(BlockFace.DOWN).getType() == Material.IRON_BLOCK
+				&& ((b.getRelative(BlockFace.DOWN).getRelative(BlockFace.EAST).getType() == Material.IRON_BLOCK
+					&& b.getRelative(BlockFace.DOWN).getRelative(BlockFace.WEST).getType() == Material.IRON_BLOCK)
+				|| (b.getRelative(BlockFace.DOWN).getRelative(BlockFace.NORTH).getType() == Material.IRON_BLOCK
+					&& b.getRelative(BlockFace.DOWN).getRelative(BlockFace.SOUTH).getType() == Material.IRON_BLOCK))) {
+			if (!e.getPlayer().hasPermission("nogolems.allow.iron")) {
+				e.getPlayer().sendMessage(ChatColor.RED + "You do not have permission to build an Iron Golem.");
+				e.setCancelled(true);
+			}
+			return;
+		}
 	}
 }
